e38aa9dd1339febe0fc33a2d53e200f7ace65880
frgarciames <fran17901@gmail.com>
2021-12-22 11:34:26 +0100

    Fix react scope

--- packages/react/src/Provider.tsx
+++ packages/react/src/Provider.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { State, initialState } from './State'
 
 import { Context } from './Context'
