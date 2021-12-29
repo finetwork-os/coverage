3d30bcb4c4719d175efce9bfd58cfcd8cf265992
frgarciames <fran17901@gmail.com>
2021-12-29 17:45:17 +0100

    Remove normalizer url

--- packages/website/src/lib/coverage.ts
+++ packages/website/src/lib/coverage.ts
@@ -3,7 +3,7 @@ import { Coverage } from '@finetwork/coverage'
 export const coverage = new Coverage({
   storage: typeof window !== 'undefined' ? localStorage : null,
   urls: {
-    normalizer: 'https://gateway-public-area.finetwork.com/coverage/normalize',
+    normalizer: '',
     locator: '',
     visibility: '',
   },
